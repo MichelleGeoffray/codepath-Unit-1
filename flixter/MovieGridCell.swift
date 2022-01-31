d4802e39d2dea93140e0b21603422cac5e3df52a
MichelleGeoffray <mgeoffra@purdue.edu>
2022-01-30 22:44:11 -0500

    unit 2 push

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by user209058 on 1/30/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
